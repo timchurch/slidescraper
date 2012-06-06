9bad5644bfc2bba657ad2866b6c73f988fe8723e
Tim Church <tim.church+github@gmail.com>
2012-06-06 18:13:26 +0200

    Adding initial setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name="slidescraper",
+    version=version,
+    maintainer='Tim Church',
+    maintainer_email='tim.church+slidescraper@gmail.com',
+    url='https://github.com/timchurch/slidescraper',
+    packages=find_packages(),
+    install_requires=[
+        'lxml>=2.3.4',
+        'oauth2>=1.5.211',
+        'feedparser>=5.1.1',
+        'beautifulsoup4>=4.0.2',
+        'requests>=0.10.8',
+        'dateutil==1.5',
+#        'xmltodict>=0.1',
+    ],
+)
+
